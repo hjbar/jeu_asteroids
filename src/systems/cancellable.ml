b531eb146cc0d3e58c5b34e2b98d06470d726cc9
hjbar <hjbar@protonmail.com>
2024-04-04 22:27:02 +0200

    Fix unless stuff in cancellable

--- src/systems/cancellable.ml
+++ src/systems/cancellable.ml
@@ -6,10 +6,7 @@ let init _ = ()
 
 let update dt el =
   let l = ref [] in
-  let c = ref 0 in
   Seq.iter
-    (fun (e1 : t) ->
-      if e1#is_offscreen#get || e1#is_dead#get then l := e1 :: !l;
-      incr c )
+    (fun (e1 : t) -> if e1#is_offscreen#get || e1#is_dead#get then l := e1 :: !l)
     el;
   List.iter (fun (e1 : t) -> e1#remove#get ()) !l
