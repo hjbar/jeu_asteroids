37dd5b3b24391b2486d545ef7d995faf580df4cf
hjbar <hjbar@protonmail.com>
2024-04-19 10:12:35 +0200

    Ident

--- src/core/print.ml
+++ src/core/print.ml
@@ -89,7 +89,7 @@ let print () =
   if Global.bonus_drop_rate () = 100 then begin
     let icon = Gfx.get_resource (Texture.get Icon_background) in
     let x = Global.width - 10 - size in
-    let y = Global.height - 2*10 - 2*size in
+    let y = Global.height - (2 * 10) - (2 * size) in
     Gfx.blit_full ctx
       (Gfx.get_surface (Global.window ()))
       icon 0 0 32 32 x y size size;
@@ -111,21 +111,24 @@ let print () =
 let pause_screen () =
   let img = Gfx.get_resource (Texture.get Pause_screen) in
   let ctx = Gfx.get_context (Global.window ()) in
-  Gfx.blit_full ctx (Gfx.get_surface (Global.window ())) img 0 0 2048 1024 0 0 Global.width Global.height;
+  Gfx.blit_full ctx
+    (Gfx.get_surface (Global.window ()))
+    img 0 0 2048 1024 0 0 Global.width Global.height;
   Gfx.commit ctx
 
 let game_over () =
-
   let ctx = Gfx.get_context (Global.window ()) in
   Gfx.set_color ctx (Gfx.color 22 196 53 255);
   let txt = Printf.sprintf "%.2f" (Scoring.get ()) in
   let s = Gfx.render_text ctx txt (Option.get !Global.big_font) in
   let w, h = Gfx.measure_text txt (Option.get !Global.big_font) in
   let x = (Global.width - w) / 2 in
-  let y = (Global.height - h) / 2 + 40 in
-  
+  let y = ((Global.height - h) / 2) + 40 in
+
   Audio.play Defeat;
   let img = Gfx.get_resource (Texture.get Final_screen) in
-  Gfx.blit_full ctx (Gfx.get_surface (Global.window ())) img 0 0 2048 1024 0 0 Global.width Global.height;
+  Gfx.blit_full ctx
+    (Gfx.get_surface (Global.window ()))
+    img 0 0 2048 1024 0 0 Global.width Global.height;
   Gfx.blit ctx (Gfx.get_surface (Global.window ())) s x y;
   Gfx.commit ctx
